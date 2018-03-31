680ff075a037a422bd3847001d1fa9d6eb64c4aa
Lili <lilizhu1998@gmail.com>
2018-03-31 03:14:46 -0700

    EventProfile
    Alomst finish the page design

--- app/src/main/java/com/example/alan/lahacks/EventProfile.java
+++ app/src/main/java/com/example/alan/lahacks/EventProfile.java
@@ -1,25 +1,33 @@
 package com.example.alan.lahacks;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class EventProfile extends AppCompatActivity {
+
+    //Initialize all of my Objects to be declared within onCreate method.
+    TextView descriptionText;
+    String descriptionTextString = "The star party will be host in front of the fountain" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_profile);
 
+        //Setup event picture
         ImageView eventPicture =(ImageView) findViewById(R.id.eventImage) ;
-        eventPicture.setImageResource(R.drawable.eclipse);
-//        /**
-//         * Set the event picture.
-//         */
-//        Resources res = getResources();
-//        Drawable drawable = res.getDrawable(R.drawable.eclipse);
+        eventPicture.setBackgroundResource(R.drawable.eclipse);
+
+        //Link all of my objects to the ones created in my XML.
+        descriptionText = (TextView)findViewById(R.id.descriptionText);
+        descriptionText.setText(descriptionTextString);
+
     }
 }
