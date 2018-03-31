776b53963f82e00c951fd57d8b50accf16541dfb
Lili <lilizhu1998@gmail.com>
2018-03-30 23:26:15 -0700

    Create activity EventProfile

---
+++ app/src/main/java/com/example/alan/lahacks/EventProfile.java
@@ -0,0 +1,13 @@
+package com.example.alan.lahacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_profile);
+    }
+}
