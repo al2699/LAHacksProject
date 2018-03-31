15c12ae7b00d584a40121abf9ec37babcb4d304a
Alan <hernandezalan299@gmail.com>
2018-03-30 21:24:11 -0700

    Init

--- app/src/main/java/com/example/alan/lahacks/MainActivity.java
+++ app/src/main/java/com/example/alan/lahacks/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Adding a change here.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
