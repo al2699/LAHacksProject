f71c2642e98db65987f26e092e3896aa65b5060c
Jesse Wolf <jessewolf42892@gmail.com>
2018-03-31 01:58:26 -0700

    Test Fix - Jesse

--- app/src/main/java/com/example/alan/lahacks/ButtonMenu.java
+++ app/src/main/java/com/example/alan/lahacks/ButtonMenu.java
@@ -1,7 +1,9 @@
 package com.example.alan.lahacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ButtonMenu extends AppCompatActivity {
 
@@ -10,6 +12,34 @@ public class ButtonMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buttonMenu);
+        setContentView(R.layout.activity_button_menu);
     }
+
+    /**
+     * toEventProfile method. Takes user to GEvent Profile screen when button is clicked.
+     * @param view View
+     */
+    public void toEventProfile(View view) {
+        Intent startNewActivity = new Intent(this, EventProfile.class);
+        startActivity(startNewActivity);
+    }
+
+    /**
+     * toPersonalProfile method. Takes user to Personal Profile screen when button is clicked.
+     * @param view View
+     */
+    public void toPersonalProfile(View view) {
+        Intent startNewActivity = new Intent(this, PersonalProfile.class);
+        startActivity(startNewActivity);
+    }
+
+    /*
+     * toGameSetup method. Takes user to Game Setup screen when button is clicked.
+     * @param view View
+
+    public void toGameSetup(View view) {
+        Intent startNewActivity = new Intent(this, GameSetup.class);
+        startActivity(startNewActivity);
+    }
+    */
 }
