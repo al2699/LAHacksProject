89c0af6728e23f98e1f0e83ae7429433ea4624ff
Lili <lilizhu1998@gmail.com>
2018-03-31 08:10:12 -0700

    EventProfile and CommentPage

--- app/src/main/java/com/example/alan/lahacks/EventProfile.java
+++ app/src/main/java/com/example/alan/lahacks/EventProfile.java
@@ -21,6 +21,34 @@ public class EventProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_profile);
 
+        //Link to creator's profile !!(CAUTION: Now just link to own profile)
+        ImageButton profileButton = (ImageButton) findViewById(R.id.imageButton);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startNewActivity = new Intent(EventProfile.this, PersonalProfile.class);
+                startActivity(startNewActivity);
+
+            }
+        });
+
+        //Add actions to Like, save, and toGo buttons.
+        final ImageButton likeButton = (ImageButton) findViewById(R.id.imageButton3) ;
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                likeButton.setBackgroundResource(R.drawable.eclipse);
+            }
+        });
+
+        final ImageButton saveButton = (ImageButton) findViewById(R.id.imageButton6) ;
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveButton.setImageResource(R.drawable.common_google_signin_btn_icon_dark);
+            }
+        });
+
         //Setup event picture
         ImageView eventPicture =(ImageView) findViewById(R.id.eventImage) ;
         eventPicture.setBackgroundResource(R.drawable.eclipse);
@@ -28,6 +56,16 @@ public class EventProfile extends AppCompatActivity {
         //Link all of my objects to the ones created in my XML.
         descriptionText = (TextView)findViewById(R.id.descriptionText);
         descriptionText.setText(descriptionTextString);
+
+        //Link comment box to comment page
+        final TextView commentBox = (TextView) findViewById(R.id.comment_box) ;
+        commentBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startNewActivity = new Intent(EventProfile.this, CommentPage.class);
+                startActivity(startNewActivity);
+            }
+        });
 
     }
 }
