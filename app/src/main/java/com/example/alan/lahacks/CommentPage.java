4ce5f674ca3e19f560830edae1ec07a4523c339c
Jesse Wolf <jessewolf42892@gmail.com>
2018-03-31 23:12:27 -0700

    Cleaned up for final prototype. Got everything to ListView and whatnot. Working on Card View and adapter.

--- app/src/main/java/com/example/alan/lahacks/CommentPage.java
+++ app/src/main/java/com/example/alan/lahacks/CommentPage.java
@@ -10,7 +10,5 @@ public class CommentPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment_page);
-
-
     }
 }
