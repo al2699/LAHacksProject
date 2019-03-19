08af4111393a4f9b543ab623f7012d8add68368c
Alan <hernandezalan299@gmail.com>
2019-03-18 21:28:10 -0700

    Added some tests. Flushed out Profile class. And added some travis-ci functionality.

--- app/src/main/java/com/example/alan/lahacks/Profile.java
+++ app/src/main/java/com/example/alan/lahacks/Profile.java
@@ -1,6 +1,10 @@
 package com.example.alan.lahacks;
 
-
+/**
+ * Class dedicated to storing user profiles in memory for display
+ * @Version: 0.5
+ * @Author: Alan & Jesse
+ */
 public class Profile
 {
     private String name;
@@ -8,6 +12,21 @@ public class Profile
     private int eventsAttended;
     //Name, number of events joined, friends
 
+    /**
+     * Default constructor
+     */
+    public Profile(){
+        this.name = null;
+        this.numberOfFriends = 0;
+        this.eventsAttended = 0;
+    }
+
+    /**
+     * Full constructor
+     * @param name            Name of the user (profile owner)
+     * @param numberOfFriends Number of friends the user has
+     * @param eventsAttended  Number of events attended by the user
+     */
     public Profile(String name, int numberOfFriends, int eventsAttended)
     {
         this.name = name;
@@ -15,26 +34,79 @@ public class Profile
         this.eventsAttended = eventsAttended;
     }
 
+    /**
+     * Copy constructor.
+     * @param other Object of type Profile who's data will be copied to fill this object
+     */
+    public Profile(Profile other){
+        this.name = other.getName();
+        this.numberOfFriends = other.getNumberOfFriends();
+        this.eventsAttended = other.getEventsAttended();
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if(other != null && other.getClass() == this.getClass()){
+            Profile otherP = (Profile) other;
+            return this.name == otherP.getName() && this.numberOfFriends == otherP.getNumberOfFriends()
+                    && this.eventsAttended == otherP.getEventsAttended();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "name='" + name + '\'' +
+                ", numberOfFriends=" + numberOfFriends +
+                ", eventsAttended=" + eventsAttended +
+                '}';
+    }
+
+    /**
+     * Sets the user's name
+     * @param name name of the user
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the number of friends the user has
+     * @param numberOfFriends number of friends the user has
+     */
     public void setNumberOfFriends(int numberOfFriends) {
         this.numberOfFriends = numberOfFriends;
     }
 
+    /**
+     * Sets the number of events attended
+     * @param eventsAttended number of events attended by user
+     */
     public void setEventsAttended(int eventsAttended) {
         this.eventsAttended = eventsAttended;
     }
 
+    /**
+     * Gets the name of the user
+     * @return the name of the user in string form
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the number of friends the given user has
+     * @return number of friends the user has
+     */
     public int getNumberOfFriends() {
         return numberOfFriends;
     }
 
+    /**
+     * Gets the number of events the user has attended
+     * @return number of events the user has attended
+     */
     public int getEventsAttended() {
         return eventsAttended;
     }
--- app/src/test/java/com/example/alan/lahacks/ExampleUnitTest.java
+++ app/src/test/java/com/example/alan/lahacks/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.alan.lahacks;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,58 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    //Profile tests
+    @Test
+    public void testProfileConstructors(){
+        Profile def = new Profile();
+        Profile prof = new Profile("Alan", 0, 1000);
+    }
+
+    //Testing toString (simple test; need to improve)
+    @Test
+    public void testToString(){
+        Profile toStringTest = new Profile("Lol", 10, 100);
+        String testString = "Profile{" +
+                "name='" + "Lol" + '\'' +
+                ", numberOfFriends=" + 10 +
+                ", eventsAttended=" + 100 +
+                '}';
+        assertEquals(toStringTest.toString(),  testString);
+    }
+
+    //Testing some setters
+    @Test
+    public void testProfileSetters(){
+        Profile ex = new Profile();
+        String testString = "Profile{" +
+                "name='" + "name" + '\'' +
+                ", numberOfFriends=" + 3 +
+                ", eventsAttended=" + 5 +
+                '}';
+        ex.setEventsAttended(10);
+        ex.setName("name");
+        ex.setEventsAttended(5);
+        ex.setNumberOfFriends(3);
+
+        assertEquals(ex.toString(),  testString);
+    }
+
+    @Test
+    public void testProfileGetters(){
+        Profile ex = new Profile();
+        ex.setEventsAttended(10);
+        ex.setName("lol");
+        ex.setNumberOfFriends(3);
+
+        //Testing getters one-by-one
+        assertEquals(ex.getName(), "lol");
+        assertEquals(ex.getEventsAttended(), 10);
+        assertEquals(ex.getNumberOfFriends(), 3);
     }
 }
