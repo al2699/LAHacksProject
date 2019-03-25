7af5f829f8df7ae7aa9d205620c64ad560601b62
Alan <hernandezalan299@gmail.com>
2019-03-25 12:13:58 -0700

    Updated python a little. Need to figure out how to push to firebase server.

--- web-scraping/WebCrawler.py
+++ web-scraping/WebCrawler.py
@@ -1,8 +1,34 @@
 #Remember string[19:-8] for removing tags from titles
-
 import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 from bs4 import BeautifulSoup
+from HTMLParser import HTMLParser
 
+
+#Dictionary mapping HTML entity types to unicode equivalents
+"""
+dictionary = {
+    "nbsp":" "
+    "lt":"<"
+    "amp":"&"
+    "quot":"\"\""
+    "apos":"\'"
+    }
+"""
+
+#Initialize firestore
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': "group-2720d",
+})
+
+db = firestore.client()
+
+#Initialize html parser
+h = HTMLParser()
 
 url = "https://25livepub.collegenet.com/calendars/miracosta-college-events.xml"
 resp = requests.get(url)
@@ -17,6 +43,10 @@ TODO: Need to figure out the parsing for parsing the date, location, and date
 from the content portion of a given xml event entry. Also need to figure how to
 map the HTML specific entities to unicode (i.e. convert &ndash; to -)
 """
+
+#Parses the title from a title string
+def parse_title(title_string):
+    return title_string[19:-8]
 
 #Parses the location
 def parse_location(content):
